4462c64b51c9f57229f7d9b262612e33321ab9f6
Emilio Wuerges <wuerges@gmail.com>
2020-05-04 17:52:16 -0300

    started priority queue

--- graph.js
+++ graph.js
@@ -1,5 +1,49 @@
 
 // global variables
-N = 30;
-M = 30;
-G = [ ];
+var N = 30;
+var M = 30;
+var G = [ ];
+
+
+// constants
+var sqrt2 = 1.4142;
+//var ds = [[0, -1, -1], [-1, 0, -1], [0, 1, -1], [1, 0, -1], [-1, -1, -2], [1, -1, -2], [-1, 1, -2] ,[1, 1, -2]]
+//var ds = [[0, -1, -1], [-1, 0, -1], [0, 1, -1], [1, 0, -1]];
+var ds = [[1, 0, -1], [0, 1, -1], [-1, 0, -1], [0, -1, -1]];
+
+
+function valid(x, y) {
+    return x < M && y < N && x >=0 && y >= 0;
+}
+
+function paint(py, px, pe, f) {    
+    
+    var visited = [];
+    for(i=0; i < N; ++i) {
+        r = [];
+        for(j=0; j < M; ++j)  {
+            r.push(false);
+        }
+        visited.push(r);
+    }
+
+    var q = [];
+    q.push([px, py, pe]);
+
+    while(q.length > 0) {
+        var u = q.pop();
+        let [x, y, energy] = u;
+        //console.log(u, x, y, energy)
+
+        visited[x][y] = true;
+        f(x, y);
+
+        for (let [dx, dy, decay] of ds) {
+            //console.log(dx, dy, decay);
+            if(energy+decay > 0 && valid(x+dx, y+dy) && !visited[x+dx][y+dy]) {                
+                q.push([x+dx, y+dy, energy+decay]);
+            }
+        }
+    }
+}
+
---
+++ queue.js
@@ -0,0 +1,38 @@
+
+function bubble_down(q, i) {
+    l = 2*i+1;
+    r = 2*i+2;
+
+    least = i;
+    if(l < q.length && q[l][0] < q[i][0]) {
+        least = l;
+    }
+    if(r < q.length && q[r][0] < q[i][0]) {
+        least = r;
+    }
+    if(least != i) {
+        qswap(q, i, least);
+        bubble_down(q, least);
+    }
+}
+
+function bubble_up(q, i) {
+    if(i > 0) {
+        p = i / 2;
+        if(q[i][0] > q[p][0]) {
+            qswap(q, i, p);
+            bubble_up(q, p);
+        }
+    }
+}
+
+function qswap(q, x, y) {
+    aux = q[x];
+    q[x] = q[y];
+    q[y] = aux;
+}
+
+function qpush(q, k, v) {
+    q.push([k, v]);
+    bubble_up(q, q.length -1);    
+}
