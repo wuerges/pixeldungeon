31a6bcedbcd24dd2977292abae27cac5115e8d69
Emilio Wuerges <wuerges@gmail.com>
2020-05-04 21:00:24 -0300

    reverting to use priority queue

--- graph.js
+++ graph.js
@@ -1,7 +1,7 @@
 
 // global variables
 var N = 30;
-var M = 30;
+var M = 50;
 var G = [ ];
 
 // constants
@@ -11,23 +11,23 @@ var ds = [[0, -1, -1], [-1, 0, -1], [0, 1, -1], [1, 0, -1], [-1, -1, -sqrt2], [1
 // var ds = [[1, 0, -1], [0, 1, -1], [-1, 0, -1], [0, -1, -1]];
 
 
-function redraw(x, y, d, f) {
-    //for(let [x, y, d] of players) {
+function redraw(points, d, f) {
+    for(let [x, y] of points) {
         for(i=0; i < N; ++i) {
-            dy = (y - i) * (y-i)
+            dy = (y-i) * (y-i);
 
             for(j=0; j < M; ++j)  {
-                dx = (x - j) * (x-j)
-                if(dx + dy < d*d) {
+                dx = (x-j) * (x-j);
+                if(dx + dy <= 1+d*d) {
                     f(i, j);
                 }                
             }
         }
-    //}
+    }
 }
 
 function valid(x, y) {
-    return x < M && y < N && x >=0 && y >= 0;
+    return x < N && y < M && x >=0 && y >= 0;
 }
 
 function paint(py, px, pe, f) {    
@@ -41,15 +41,15 @@ function paint(py, px, pe, f) {
         visited.push(r);
     }
 
-    var q = [];
+    var pq = [];
     
-    q.push([px, py, pe]);
+    qpush(pq, pe, [px, py, pe]);
 
-    while(q.length > 0) {
+    while(pq.length > 0) {
         // var u = qpop(pq);
-        var u = q.pop();
+        var u = qpop(pq);
         let [x, y, energy] = u;
-        console.log(u, x, y, energy)
+        //console.log(u, x, y, energy)
 
         visited[x][y] = energy;
         f(x, y);
@@ -57,7 +57,7 @@ function paint(py, px, pe, f) {
         for (let [dx, dy, decay] of ds) {
             //console.log(dx, dy, decay);
             if(energy+decay > 0 && valid(x+dx, y+dy) && energy+decay > visited[x+dx][y+dy]) {                
-                q.push([x+dx, y+dy, energy+decay]);
+                qpush(pq, energy+decay, [x+dx, y+dy, energy+decay]);
             }
         }
     }
